fccc9491a0b799b01a48840d631e6b3f62d6e062
Grégory Marini <greg.marini@hotmail.fr>
2023-04-16 14:44:18 +0200

    add users route

--- server/index.js
+++ server/index.js
@@ -9,6 +9,8 @@ import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import authRoutes from './routes/auth.routes.js';
+import userRoutes from './routes/users.routes.js';
+
 import { register } from './controllers/auth.controllers.js';
 
 //----------- CONFIGURATIONS
@@ -43,6 +45,7 @@ app.post('/auth/register', upload.single('picture'), register);
 
 // ROUTES
 app.use('/auth', authRoutes);
+app.use("/users", userRoutes);
 
 // MONGOOSE SET UP
 const PORT = process.env.PORT || 6001;
