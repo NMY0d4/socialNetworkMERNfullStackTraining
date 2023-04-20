d82c398e4ed26f29df8097d46424a09e53ba95f0
Grégory Marini <greg.marini@hotmail.fr>
2023-04-20 10:54:34 +0200

    work on login page (form with formik)

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,5 +1,39 @@
+import { Box, Typography, useTheme, useMediaQuery } from '@mui/material';
+import Form from 'form';
+
 function LoginPage() {
-  return <div>LoginPage</div>;
+  const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery('(min-width: 1020px)');
+  return (
+    <Box
+      width='100%'
+      backgroundColor={theme.palette.background.alt}
+      p='1rem 6%'
+    >
+      <Box>
+        <Typography
+          fontWeight='bold'
+          fontSize='32px'
+          color='primary'
+          textAlign='center'
+        >
+          Sociopedia
+        </Typography>
+      </Box>
+      <Box
+        width={isNonMobileScreens ? '50%' : '93%'}
+        p='2rem'
+        m='2rem auto'
+        borderRadius='1.5rem'
+        backgroundColor={theme.palette.background.alt}
+      >
+        <Typography fontWeight='500' variant='h5' sx={{ mb: '1.5' }}>
+          Welcome to Sociapedia, the Social Media for Sociopaths!
+        </Typography>
+        <Form />
+      </Box>
+    </Box>
+  );
 }
 
 export default LoginPage;
