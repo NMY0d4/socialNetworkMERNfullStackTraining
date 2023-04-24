5664a0e8ede9de8996f1c3d2acf6e5ef5197411d
Grégory Marini <greg.marini@hotmail.fr>
2023-04-24 16:04:44 +0200

    fix likes bug(state write 2 times...)

--- client/src/state/index.js
+++ client/src/state/index.js
@@ -32,13 +32,13 @@ export const authSlice = createSlice({
     setPosts: (state, action) => {
       state.posts = action.payload.posts;
     },
-  },
-  setPost: (state, action) => {
-    const updatedPosts = state.state.posts.map((post) => {
-      if (post._id === action.payload.post_id) return action.payload.post;
-      return post;
-    });
-    state.posts = updatedPosts;
+    setPost: (state, action) => {
+      const updatedPosts = state.posts.map((post) => {
+        if (post._id === action.payload.post._id) return action.payload.post;
+        return post;
+      });
+      state.posts = updatedPosts;
+    },
   },
 });
 
