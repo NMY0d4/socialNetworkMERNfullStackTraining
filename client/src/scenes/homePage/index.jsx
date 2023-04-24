b96e108dcae0b2d8ca0e47d937071869a622c45c
Grégory Marini <greg.marini@hotmail.fr>
2023-04-24 17:20:42 +0200

    add FriendListWidget

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -5,6 +5,7 @@ import UserWidget from 'scenes/widgets/UserWidget';
 import MyPostWidget from 'scenes/widgets/MyPostWidget';
 import PostsWidget from 'scenes/widgets/PostsWidget';
 import AdvertWidget from 'scenes/widgets/AdvertWidget';
+import FriendListWidget from 'scenes/widgets/FriendListWidget';
 
 function HomePage() {
   const isNonMobileScreens = useMediaQuery('(min-width:1020px)');
@@ -33,8 +34,8 @@ function HomePage() {
           <Box flexBasis='26%'>
             <AdvertWidget />
             <Box m='2rem 0' />
+            <FriendListWidget userId={_id} />
           </Box>
-          
         )}
       </Box>
     </Box>
