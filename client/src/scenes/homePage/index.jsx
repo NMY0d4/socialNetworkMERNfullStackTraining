eb3e48cc5ebbde1bb44d6bae2cf36ec9bc55ed92
Grégory Marini <greg.marini@hotmail.fr>
2023-04-24 16:49:17 +0200

    add AdvertWidget

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -4,6 +4,7 @@ import Navbar from 'scenes/navbar';
 import UserWidget from 'scenes/widgets/UserWidget';
 import MyPostWidget from 'scenes/widgets/MyPostWidget';
 import PostsWidget from 'scenes/widgets/PostsWidget';
+import AdvertWidget from 'scenes/widgets/AdvertWidget';
 
 function HomePage() {
   const isNonMobileScreens = useMediaQuery('(min-width:1020px)');
@@ -28,7 +29,13 @@ function HomePage() {
           <MyPostWidget picturePath={picturePath} />
           <PostsWidget userId={_id} />
         </Box>
-        {isNonMobileScreens && <Box flexBasis='26%'></Box>}
+        {isNonMobileScreens && (
+          <Box flexBasis='26%'>
+            <AdvertWidget />
+            <Box m='2rem 0' />
+          </Box>
+          
+        )}
       </Box>
     </Box>
   );
